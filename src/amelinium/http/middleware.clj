41f26da8a91e7344d960a6785ce11a3183885a56
Paweł Wilk <siefca@gnu.org>
2022-05-15 16:22:12 +0200

    Middleware chain names are now set to the values of configuration keys

--- src/amelinium/http/middleware.clj
+++ src/amelinium/http/middleware.clj
@@ -19,12 +19,12 @@
 (defn generic
   "Generic middleware which renders results by calling configured web handler
   function (`:post` key of `config` if `config` is a map or a value of `config`)."
-  [config]
+  [k config]
   (let [web-handler-sym (if (map? config) (:post config) config)
         web-handler     (var/deref-symbol web-handler-sym)]
     (when web-handler
       (log/msg "Installing generic web handler:"  web-handler-sym)
-      {:name    ::renderer
+      {:name    (or (keyword k) ::renderer)
        :compile (fn [data opts]
                   (fn [handler]
                     (fn [req]
@@ -33,12 +33,12 @@
 
 (defn prep
   "Generic middleware which prepares a request for the controller."
-  [config]
+  [k config]
   (let [preparer-sym (if (map? config) (:pre config) config)
         preparer     (var/deref-symbol preparer-sym)]
     (when preparer
       (log/msg "Installing generic request preparer:"  preparer-sym)
-      {:name    ::preparer
+      {:name    (or  (keyword k) ::preparer)
        :compile (fn [data opts]
                   (fn [handler]
                     (fn [req]
@@ -55,10 +55,10 @@
 (system/add-init  ::chain    [k config] (var/make k (prep-chain config)))
 (system/add-halt! ::chain    [k config] (var/make k nil))
 
-(system/add-init  ::generic   [_ config] (generic config))
+(system/add-init  ::generic   [k config] (generic k config))
 (system/add-halt! ::generic   [_ config] nil)
 
-(system/add-init  ::prep      [_ config] (prep config))
+(system/add-init  ::prep      [k config] (prep k config))
 (system/add-halt! ::prep      [_ config] nil)
 
 (derive ::default ::chain)
