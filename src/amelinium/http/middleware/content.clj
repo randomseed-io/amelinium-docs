299c055fa283f6f190ff4085a7aa3c3e983942b7
Paweł Wilk <siefca@gnu.org>
2022-06-18 02:37:38 +0200

    Content types middleware can now initialize language settings using language configuration map

--- src/amelinium/http/middleware/content.clj
+++ src/amelinium/http/middleware/content.clj
@@ -15,8 +15,6 @@
             [io.randomseed.utils.var :as       var]
             [io.randomseed.utils     :refer   :all]))
 
-(defonce settings nil)
-
 (defn wrap-accept
   "Content types handling middleware."
   [k config]
@@ -29,17 +27,33 @@
                     (fn [req]
                       (handler req)))))}))
 
+(defn prep-language
+  [config]
+  (if (or (nil? (valuable config)) (system/ref? config))
+    config
+    (let [language (valuable (:language config))]
+      (if (or (nil? language) (system/ref? language))
+        config
+        (let [default   (vec/of-strings (:default-language config) system/ref?)
+              default   (first default)
+              supported (if (map? language) (:supported language) language)
+              supported (vec/of-strings supported system/ref?)
+              supported (if (or (nil? default) (system/ref? supported)
+                                (some #(and (some? %) (= default %)) supported))
+                          supported
+                          (conj (or supported []) default))]
+          (assoc config :default-language default :language supported))))))
+
 (defn prep-accept
   [config]
   (if-not (map? config)
     config
     (-> config
+        prep-language
         (update :mime     vec/of-strings system/ref?)
-        (update :language vec/of-strings system/ref?)
         (update :charset  vec/of-strings system/ref?)
         (update :encoding vec/of-strings system/ref?)
         (update :mime     (fnil identity ["text/html"]))
-        (update :language (fnil identity ["en"]))
         (update :charset  (fnil identity ["utf-8"]))
         (update :encoding (fnil identity ["identity"])))))
 
