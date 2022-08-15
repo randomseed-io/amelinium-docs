9a8e2cd1e41e9782ba88c8a9e0c0ad8db787e7ad
Paweł Wilk <siefca@gnu.org>
2022-08-15 14:21:43 +0200

    Inline docs updated

--- src/amelinium/web/controller.clj
+++ src/amelinium/web/controller.clj
@@ -341,15 +341,18 @@
   middleware chain. Takes exception object `e`, response wrapper `respond` and
   `raise` function.
 
-  When a coercion error is detected during request processing, it creates a sequence
-  of maps containing coercion errors (with `amelinium.http.middleware.coercion/map-errors`).
-  If there is a session this map it is stored in a session variable `:form-errors`
-  under the `:errors` key (additionally, there is a `:dest` key identifying a path
-  of the current page). If there is no valid session or a session variable cannot be
-  stored, the result is serialized to a query string parameter `form-errors`
-  with erroneous fields separated by commas or tuples of fields containing field types
-  (if type information can be obtained from the exception data), and joined by colon
-  characters, like `login:email,password:password` or simply `login,password`.
+  When a coercion error is detected during request processing, it creates a map (by
+  calling `amelinium.http.middleware.coercion/map-errors`) containing parameter
+  identifiers associated with parameter types (or with `nil` values if type
+  information is not available). If there is a session then this map it is stored in
+  a session variable `:form-errors` under the `:errors` key (additionally, there is a
+  `:dest` key identifying a path of the current page). If there is no valid session
+  or a session variable cannot be stored, the result is serialized as a query string
+  parameter `form-errors` with erroneous fields separated by commas. If type name is
+  available then a string in a form of `parameter:type` is generated. If type name is
+  not available, a simple parameter name is generated. So the example value (before
+  encoding) may look like `email,secret:password` (`email` parameter without type
+  information, `secret` parameter with type named `password`).
 
   Next, the originating URI is obtained from `Referer` header and a temporary
   redirect (with HTTP code 307) is generated with this path and a query string
@@ -358,19 +361,21 @@
   overriden by the `:form-errors/page` configuration option associated with HTTP
   route data.
 
-  If the destination URI cannot be established or if coercion error happened during
+  If the destination URI cannot be established or if a coercion error happened during
   handling some previous coercion error (so current page is where the browser had
   been redirected to) then instead of generating a redirect, a regular page is
-  rendered with HTTP code 422. The `:app/data` key of a request map is updated with
-  `:title` (associated with translated message of `:error/parameters`) and
-  `:form/errors` (associated with the result of calling
-  `amelinium.http.middleware.coercion/explain-errors`).
+  rendered with HTTP code of 422. The `:app/data` key of a request map is updated
+  with:
+
+  - `:title` (translated message of `:error/parameters`),
+  - `:form/errors` (result of `amelinium.http.middleware.coercion/map-errors`),
+  - `:coercion/errors` (result of `amelinium.http.middleware.coercion/explain-errors`).
 
   When a coercion error is detected during response processing, a web page of HTTP
-  code 500 is rendered. The `:app/data` key of a request map is updated with the
-  `:title` (associated with translated message of `:error/parameters`) and
-  `:form/errors` (associated with the result of calling
-  `amelinium.http.middleware.coercion/explain-errors`)."
+  code 500 is rendered. The `:app/data` key of a request map is updated with the:
+
+  - `:title` (translated message of `:error/parameters`),
+  - `:form/errors` (result of `amelinium.http.middleware.coercion/explain-errors`)."
   [e respond raise]
   (let [data  (ex-data e)
         req   (get data :request)
