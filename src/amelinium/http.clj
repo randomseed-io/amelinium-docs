62d26f24a62c68b9fd0a1a006dd353193a0f6530
Paweł Wilk <siefca@gnu.org>
2022-08-26 13:34:28 +0200

    Protocol-based single-method dispatch for amelinium.http/get-route-data

--- src/amelinium/http.clj
+++ src/amelinium/http.clj
@@ -13,23 +13,76 @@
             [reitit.core                     :as        r]
             [reitit.ring                     :as     ring]
             [io.randomseed.utils             :refer  :all]
-            [io.randomseed.utils.reitit.http :as     http]))
+            [io.randomseed.utils.reitit.http :as     http])
+
+  (:import [reitit.core Match]))
 
 (p/import-vars [io.randomseed.utils.reitit.http
                 router? router match? match
-                route-data route-data-param route-name
+                route-data-param route-name
                 route-middleware route-handler route-conflicting?
                 path req-or-route-param])
+
+(defprotocol Datable
+  (get-route-data [req-or-match] [req-or-match param] [req match param]))
+
+(extend-protocol Datable
+
+  Match
+
+  (get-route-data
+    ([req-or-match]
+     (get req-or-match :data))
+    ([req-or-match param]
+     (get (get req-or-match :data) param))
+    ([match req param]
+     (get (or (get match :data)
+              (get req :route/data)
+              (get (get req ::r/match) :data))
+          param)))
+
+  clojure.lang.IPersistentMap
+
+  (get-route-data
+    ([req-or-match]
+     (or (get req-or-match :route/data)
+         (get (get req-or-match ::r/match) :data)))
+    ([req-or-match param]
+     (get (or (get req-or-match :route/data)
+              (get (get req-or-match ::r/match) :data))
+          param))
+    ([req match param]
+     (get (or (get req :route/data)
+              (get match :data)
+              (get (get req ::r/match) :data))
+          param)))
+
+  clojure.lang.Associative
+
+  (get-route-data
+    ([req-or-match]
+     (or (get req-or-match :route/data)
+         (get (get req-or-match ::r/match) :data)))
+    ([req-or-match param]
+     (get (or (get req-or-match :route/data)
+              (get (get req-or-match ::r/match) :data))
+          param))
+    ([req match param]
+     (get (or (get req :route/data)
+              (get match :data)
+              (get (get req ::r/match) :data))
+          param)))
+
+  nil
+
+  (get-route-data
+    ([req-or-match]
+     nil)
+    ([req-or-match param]
+     nil)
+    ([req match param]
+     (if (nil? match) nil (get-route-data match req param)))))
 
 (defn inject-route-data
   [req]
   (assoc req :route/data (get (get req ::r/match) :data)))
-
-(defn get-route-data
-  ([req-or-match]
-   (or (get req-or-match :route/data)
-       (route-data req-or-match)))
-  ([req-or-match param]
-   (get (get-route-data req-or-match) param))
-  ([req match param]
-   (get (or (get req :route/data) (route-data match)) param)))
