f3aeaa737b88b19b0c0948a7c02080b319650d01
Paweł Wilk <siefca@gnu.org>
2022-09-18 01:24:38 +0200

    Amelinium.errors refactored (function names, error rendering)

--- src/amelinium/errors.clj
+++ src/amelinium/errors.clj
@@ -42,31 +42,62 @@
   [v]
   (satisfies? ErrorsConfigurable v))
 
-(defn to-response
-  "Returns a response function for the given `config` (which may be of type
-  `ErrorsConfig`, a request map, or a `Match` object) and `error` (expressed as
-  a keyword)."
-  ([config-src error]
-   (to-response config error nil))
-  ([config-src error default]
-   (if error
-     (if-some [config (config config-src)]
-  ([config-src error]
-   (to-view config error nil))
-  ([config-src error default]
-   (if error
-     (if-some [config (config config-src)]
-       (or (.views ^ErrorsConfig config) default)))))
-
 (defn most-significant
   "Returns a most significant error from the given `errors` using the given
   `config` (which may be of type `ErrorsConfig`, a request map, or a `Match`
-  object)."
+  object). If `errors` is not a sequence but a keyword, it is returned as-is. Returns
+  `nil` when `config-src` or `errors` is `nil`."
   [config-src errors]
   (if errors
-    (if-some [config (config config-src)]
-      (or (some errors (.priorities ^ErrorsConfig config))
-          (first errors)))))
+    (if (keyword? errors)
+      errors
+      (if-some [config (config config-src)]
+        (or (some errors (.priorities ^ErrorsConfig config))
+            (first errors))))))
+
+(defn render-fn
+  "Gets a response rendering function using the given configuration source `config-src`
+  (which may be of type `ErrorsConfig`, a request map, or a `Match` object) and
+  `error` (expressed as a keyword). Returns `nil` when `config-src` or `error` is
+  `nil`."
+  ([config-src error]
+   (render-fn config-src error nil))
+  ([config-src error default]
+   (if error
+     (if-some [config (config config-src)]
+       (or (get (.responses ^ErrorsConfig config) (most-significant config error))
+           default)))))
+
+(defn default-response
+  "Returns the default error response rendering function. Returns `nil` when
+  `config-src` is `nil`."
+  [config-src]
+  (if-some [config (config config-src)]
+    (.default-response ^ErrorsConfig config)))
+
+(defn render
+  "Renders an error response using `render-fn`. If the response rendering function
+  cannot be established, configuration default is used. Returns `nil` when
+  `config-src` is `nil`."
+  ([config-src]
+   (render config-src nil nil))
+  ([config-src error]
+   (render config-src error nil))
+  ([config-src error default]
+   (if-some [config (config config-src)]
+     (if-some [render-fn (or (render-fn config error default)
+                             (.default-response ^ErrorsConfig config))]
+       (render-fn))))
+  ([config-src error default req]
+   (if-some [config (config config-src)]
+     (if-some [render-fn (or (render-fn config error default)
+                             (.default-response ^ErrorsConfig config))]
+       (render-fn req))))
+  ([config-src error default req & more]
+   (if-some [config (config config-src)]
+     (if-some [render-fn (or (render-fn config error default)
+                             (.default-response ^ErrorsConfig config))]
+       (apply render-fn req more)))))
 
 (defn specific-id
   "Makes errors `errors` more specific by replacing generic bad ID error (as a keyword)
@@ -96,8 +127,8 @@
 (system/add-init  ::settings [_ config] (init-errors config))
 (system/add-halt! ::settings [_ config] nil)
 
-(derive ::settings   ::web)
-(derive ::settings   ::api)
-(derive ::settings   ::all)
-(derive ::priotities ::system/value)
+(derive ::web        ::settings)
+(derive ::api        ::settings)
+(derive ::all        ::settings)
+(derive ::priorities ::system/value)
 (derive ::responses  ::system/value)
